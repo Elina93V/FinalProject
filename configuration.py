21e2a4060c28631cbf4951a9459eb79ea769945c
Elina93V <lialka.elina@gmail.com>
2024-11-16 22:40:47 +0300

    Add files via upload

--- configuration.py
+++ configuration.py
@@ -1,2 +1,3 @@
-URL_SERVICE =  "https://6867a808-75f1-44e3-ba87-63e2b6b38d61.serverhub.praktikum-services.ru"
+URL_SERVICE =  "https://453f2107-5b91-4c91-b633-c3e03f2496b5.serverhub.praktikum-services.ru"
 CREAT_ORDERS = "/api/v1/orders"
+ORDER_INFORMATION = "/api/v1/orders/track?t="
